05478aba23120b0ec9d9cec613f397384b4646e0
Krisztian Kovacs <phariax74@gmail.com>
2018-11-06 14:18:54 +0100

    merged junk removed

--- scripts/sensor.lua
+++ scripts/sensor.lua
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 Sensor = {}
 Sensor.__index = Sensor
 
@@ -18,29 +18,3 @@ end
 function Sensor:GetNumColls()
     return getNumCollisions(self.object)
 end
-
-
-=======
-Sensor = {}
-Sensor.__index = Sensor
-
-function Sensor:new(ref)
-
-    local newSensor = {}
-    setmetatable(newSensor, Sensor)
-
-    newSensor.object = ref
-
-
-    return newSensor
-
-end
-
-
-function Sensor:GetNumColls()
-    return getNumCollisions(self.object)
-end
-
-
->>>>>>> 4517f21ed8907ebabc995061a274ce56b88f7590
-    
